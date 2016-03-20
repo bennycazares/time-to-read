5dd62849fd99635ef8bbd819a3aaf6e96a1e92ee
Benito Cazares IV <Benny@benitos-mbp.attlocal.net>
2016-03-19 19:46:24 -0500

    final

---
+++ time-to-read.js
@@ -0,0 +1,22 @@
+$(window).ready(function(){
+  var fetchData = $('p').text();
+  var wordsPerMinute = 230;
+
+  var timeToRead = function(fetchData, wordsPerMinute){
+    wordCount = fetchData.split(" ").length;
+    timeToRead = wordCount / wordsPerMinute;
+    timeToRead = Math.round(timeToRead);
+    return timeToRead;
+  };
+
+  console.log("Approximately " + timeToRead(fetchData, wordsPerMinute) + " minutes to read");
+  readingTime = timeToRead;
+
+  var newHTML = '<p>' + readingTime + ' minutes to read</p>';
+  var $htmlTag = $('time-to-read-view');
+
+  console.log(newHTML);
+
+  $('.time-to-read-view').append(newHTML);
+
+});
